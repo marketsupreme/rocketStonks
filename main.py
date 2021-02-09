37e413a99d42589853411db841b57df8733e89c9
Michael Walters <mike855@outlook.com>
2021-02-09 12:23:52 -0600

    updated repo with Fraij's template

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# ---------------------------
+# projects/IDB3/main.py
+# Fares Fraij
+# ---------------------------
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# ------------
+# index
+# ------------
+@app.route('/')
+def index():
+	return render_template('hello.html')
+
+# ------------
+# book
+# ------------	
+@app.route('/book/')
+def book():
+	return render_template('book.html')
+
+# debug=True to avoid restart the local development server manually after each change to your code. 
+# host='0.0.0.0' to make the server publicly available. 
+if __name__ == "__main__":
+	app.run(debug=True, host='0.0.0.0')
