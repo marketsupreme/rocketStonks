c7c8352f76714af632b7aaa24fb99c620d5114b9
Danfeng Wang <dannywang@utexas.edu>
2021-02-13 18:13:17 -0500

    Start working on the projection pages; finish basic setup; half way finish the apple inc page;

--- main.py
+++ main.py
@@ -12,18 +12,29 @@ app = Flask(__name__)
 # ------------
 # index
 # ------------
+
+
 @app.route('/')
 def index():
-	return render_template('hello.html')
+    return render_template('hello.html')
 
 # ------------
 # book
-# ------------	
+# ------------
+
+
 @app.route('/book/')
 def book():
-	return render_template('book.html')
+    return render_template('book.html')
 
-# debug=True to avoid restart the local development server manually after each change to your code. 
-# host='0.0.0.0' to make the server publicly available. 
+
+# projections page
+@app.route('/projections/')
+def projections():
+    return render_template('projections.html')
+
+
+# debug=True to avoid restart the local development server manually after each change to your code.
+# host='0.0.0.0' to make the server publicly available.
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
