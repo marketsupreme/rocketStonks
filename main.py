c96529e9df76dc2b6ecfd3847f9d8e0fed3ef470
Danfeng Wang <dannywang@utexas.edu>
2021-02-25 22:47:30 -0500

    added some bootstrap style

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ def stock():
 
 @app.route('/stock/AAPL/')
 def AAPL():
-    return  render_template('nav_bar.html') + render_template('aapl.html')
+    return render_template('nav_bar.html') + render_template('aapl.html')
 
 
 @app.route('/stock/HON/')
 def HON():
-    return  render_template('nav_bar.html') + render_template('hon.html')
+    return render_template('nav_bar.html') + render_template('hon.html')
 
 
 @app.route('/stock/MRNA/', methods=['GET', 'POST'])
 def MRNA():
-    return  render_template('nav_bar.html') + render_template('mrna.html')
+    return render_template('nav_bar.html') + render_template('mrna.html')
 
 
 @app.route('/cat/')
@@ -65,26 +65,29 @@ def catTech():
 # Projections page
 @app.route('/projections/AAPL')
 def projections_AAPL():
-    return render_template('projections_AAPL.html')
+    return render_template('nav_bar.html') + render_template('projections_AAPL.html')
 
 
 @app.route('/projections/HON')
 def projections_HON():
-    return render_template('projections_HON.html')
+    return render_template('nav_bar.html') + render_template('projections_HON.html')
 
 
 @app.route('/projections/MRNA')
 def projections_MRNA():
-    return render_template('projections_MRNA.html')
+    return render_template('nav_bar.html') + render_template('projections_MRNA.html')
 
 
 @app.route('/about/')
 def about():
     return render_template('nav_bar.html') + render_template('about.html')
 
+
 @app.route('/dummy/')
 def dummy():
     return render_template('nav_bar.html') + render_template('dummy_link.html')
+
+
 # debug=True to avoid restart the local development server manually after each change to your code.
 # host='0.0.0.0' to make the server publicly available.
 if __name__ == "__main__":
