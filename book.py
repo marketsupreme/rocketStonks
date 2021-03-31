218bf6f30875061d6dcbf3b988835427f9469f28
Michael Walters <mike855@outlook.com>
2021-03-31 13:27:25 -0500

    scrape.py and stocks.json created. still need crypto and penny stocks tickers

--- book.py
+++
@@ -1,6 +0,0 @@
-# This code generate the corresponding json file of the dict "sample"
-import json
-sample = {'Books': [{'title': 'Software Engineering', 'id': '1'}, {'title':'Algorithm Design', 'id':'2'},{'title':'Python', 'id':'3'}]}
-
-with open('books.json', 'w') as fp:
-    json.dump(sample, fp, indent=4)
