e7a8be398f6eb9633445ef4ff567d9348d492b57
Nam Nguyen <namnguyen430@gmail.com>
2021-03-31 19:42:29 -0500

    basic layout of unittest created

---
+++ unittests.py
@@ -0,0 +1,13 @@
+import unittest
+import json
+
+import app 
+
+class Tests(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual()
+
+if __name__ == "__main__":
+    with app.app.app_context():
+        unittest.main()
