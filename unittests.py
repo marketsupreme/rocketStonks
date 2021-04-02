b153b26b860a68db96a43ff473a9aec3eaf2e4f5
Nam Nguyen <namnguyen430@gmail.com>
2021-04-02 02:22:54 -0500

    created unit test button

--- unittests.py
+++ unittests.py
@@ -1,13 +1,21 @@
+import os
 import unittest
-import json
+import sys
 
-import app 
+from models import app,db,Stock
 
 class Tests(unittest.TestCase):
 
     def test_1(self):
-        self.assertEqual()
+        a = Stock(id='', title= '')
+        db.session.add(a)
+        db.session.commit()
+
+        b = db.session.query(Stock).filter_by(id = '').one()
+        self.assertEqual(str(b.id), '')
+
+        db.session.query(Stock).filter_by(id = '').delete()
+        db.session.commit()
 
 if __name__ == "__main__":
-    with app.app.app_context():
-        unittest.main()
+    unittest.main()
