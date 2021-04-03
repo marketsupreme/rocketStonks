4510cd7f48ab2c5fc775e2e533eda9afea1fa8df
Michael Walters <mike855@outlook.com>
2021-04-03 13:35:03 -0500

    postgres->postgresql in DB string

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ import os
 
 app = Flask(__name__, static_folder="./static", template_folder="./templates")
 # the uri below is used for GCP
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING",'postgres://postgres:1234@34.66.136.81:5432/postgres')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING",'postgresql://postgres:1234@34.66.136.81:5432/postgres')
 # make sure to type in your password to connect properly
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING", 'postgresql://postgres:Password@localhost:5432/postgres')
 # to suppress a warning message
