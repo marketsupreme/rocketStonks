8dae989afe2b0bbc88b54067515d4cf1ccb3284b
Allan Castaneda <acasta2399@gmail.com>
2021-04-23 11:49:39 -0500

    Added create_db2 and models2 to create stock intraday table. Fix #36.

--- models2.py
+++ models2.py
@@ -11,9 +11,9 @@ import os
 
 app = Flask(__name__, static_folder="./static", template_folder="./templates")
 # the uri below is used for GCP
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING",'postgres://postgres:1234@34.66.136.81:5432/postgres')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING",'postgres://postgres:1234@34.66.136.81:5432/postgres')
 # make sure to type in your password to connect properly
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING", 'postgresql://postgres:Ac1999&&@localhost:5432/postgres')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING", 'postgresql://postgres:Password@localhost:5432/postgres')
 # to suppress a warning message
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
